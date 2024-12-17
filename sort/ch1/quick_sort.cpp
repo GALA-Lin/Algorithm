4ac4c4f8b987fb5c8efbd25be9827c884196f5ce
Linsen HU <1563883475@qq.com>
2024-12-17 19:26:21 +0800

    new

--- sort/ch1/quick_sort.cpp
+++ sort/ch1/quick_sort.cpp
@@ -4,14 +4,14 @@ using namespace std;
 void quick_sort(int arr[], int left, int right) {
     if (left >= right) return;
     int pivot = arr[left];
-    int i = left + 1, j = right;
+    int i = left, j = right;
     while (i <= j) {
         while (i <= j && arr[i] <= pivot) i++;
         while (i <= j && arr[j] >= pivot) j--;
         if (i < j) swap(arr[i], arr[j]);
     }
     swap(arr[left], arr[j]);
-    quick_sort(arr, left, j - 1);
+    quick_sort(arr, left, j);
     quick_sort(arr, j + 1, right);
 }
 
